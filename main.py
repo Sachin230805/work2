8729bd163e87ecc7a45a2e13eff85ddfa1c4b4b2
Sachin230805 <sachinfeverr@gmail.com>
2024-07-11 11:13:22 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI,Body,status,HTTPException,Depends
+from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
+import time
+ 
+
+app = FastAPI()
+
+origins=["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+ 
+class User(BaseModel):
+    name: str
+    age: int
+    company_name: str = None
+    id:int
+try:
+     conn=psycopg2.connect(host='localhost',database='fastapi',user='postgres',password='Sachin@123',cursor_factory=RealDictCursor)
+     cursor=conn.cursor()
+     print("connection was successful")
+except Exception as error:
+     print("connection was unsuccessful")
+
+
+users=[{"name":"SACHIN","age":18,"company_name":"SEEDFLEX","id":10},{"name":"WADZEE","age":23,"company_name":"SEEDFLEX","id":12},
+      {"name":"ADITYA","age":36,"company_name":"SEEDFLEX","id":13},{"name":"ADAM","age":28,"company_name":"SEEDFLEX","id":14},
+      {"name":"GEORGE","age":37,"company_name":"SEEDFLEX","id":15},{"name":"MUSTAFA","age":53,"company_name":"SEEDFLEX","id":16}]
+     
+
+@app.get("/")
+def main_page():
+    return("WELCOME TO SEEDFLEX")
